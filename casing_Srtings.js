408a3a41d605dbf4518abee538e58c7c18f006b1
Sneha S <snehasushatha@gmail.com>
2024-05-25 21:03:47 +0400

    camel casing

---
+++ casing_Srtings.js
@@ -0,0 +1,12 @@
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+// https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/javascript
+
+String.prototype.toJadenCase = function () {
+  return this.split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
